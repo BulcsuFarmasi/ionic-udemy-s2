441fa548ef7fcf90219bf21072fe8b26586c0db0
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-01 17:50:47 +0200

    add pop to root

--- src/pages/users/user/user.ts
+++ src/pages/users/user/user.ts
@@ -20,6 +20,7 @@ export class UserPage implements OnInit {
     }
 
     onGoBack () {
-        this.navController.pop();
+        //this.navController.pop();
+        this.navController.popToRoot();
     }
 }
