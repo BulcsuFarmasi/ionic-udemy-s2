e4e51aeed4809a9535d602505609518abfd6d2bd
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-02 11:51:56 +0200

    add lifecycle hooks to users page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular'
 
 import { UsersPage } from '../users/users';
 
@@ -8,4 +9,11 @@ import { UsersPage } from '../users/users';
 })
 export class HomePage {
     public userPage = UsersPage;
+
+    constructor (private navController:NavController){}
+
+    onGoToUsers () {
+        this.navController.push(this.userPage)
+            .catch((error) => console.log(`Acces denied, argument was ${error}`));
+    }
 }
